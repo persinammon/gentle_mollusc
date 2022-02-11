88f30a4b1b2f1ccaf29edb28614e75b4fc681837
unknown <mk3739@gmail.com>
2022-02-10 21:03:13 -0800

    fix pt7

--- assets/js/hodgepodge.js
+++ assets/js/hodgepodge.js
@@ -12,10 +12,10 @@ words = [
 
 //img link, html to inject
 me = [
-["../images/about/baby.jpg", "<p><b>Likes:</b> Lion King, ants, apple juice, game where fit shapes into holes</p><p><b>Dislikes:</b> Toothbrush</p>"],
-["../images/about/17.jpg", "<a href='https://earthzine.org/fire-and-ice-wars-battles-in-the-sierra-nevada-mountains/' target='_blank'>Environmental science</a> and loads of <a href='https://www.github.com/fantastic-books-in-clips' target='_blank'>books.</a>"],
-["../images/about/23.jpg", "Working in a foreign country and wandering into a showing of <a href='https://www.youtube.com/watch?v=WQ_Yo06kIIA' target='_blank'>Hausu</a>. I want to paint in Japan and make a kalimba."],
-["../images/about/rooster.jpg", "It's a rooster."],
+["../assets/images/about/baby.jpg", "<p><b>Likes:</b> Lion King, ants, apple juice, game where fit shapes into holes</p><p><b>Dislikes:</b> Toothbrush</p>"],
+["../assets/images/about/17.jpg", "<a href='https://earthzine.org/fire-and-ice-wars-battles-in-the-sierra-nevada-mountains/' target='_blank'>Environmental science</a> and loads of <a href='https://www.github.com/fantastic-books-in-clips' target='_blank'>books.</a>"],
+["../assets/images/about/23.jpg", "Working in a foreign country and wandering into a showing of <a href='https://www.youtube.com/watch?v=WQ_Yo06kIIA' target='_blank'>Hausu</a>. I want to paint in Japan and make a kalimba."],
+["../assets/images/about/rooster.jpg", "It's a rooster."],
 ];
 
 art_wall = [
